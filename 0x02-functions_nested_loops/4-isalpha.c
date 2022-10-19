ca4b31513e942cfdfe27fd03fef68c98e44a103d
Cwaysi Morison <cwaysimorison@outlook.com>
2022-10-19 12:58:39 -0700

    hitt

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main"
+
+/**
+ * _isalpha - Entry point
+ * @c: this is the parameter
+ * Description: checking if its an alphabet whether lower or uper
+ * Return: 1 or 0 depending on result
+ */
+
+int _isalpha(int c)
+{
+	char alp, upp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		for (upp = 'A'; upp <= 'Z'; upp++)
+		{
+			if (c == alp || c == upp)
+			{
+				return (1);
+			}
+		}
+
+	}
+	return (0);
+}
