eb1cc9cc3805d58e730c72b9bd4be81d6915dde2
Cwaysi Morison <cwaysimorison@outlook.com>
2022-10-19 01:53:29 -0700

    ion

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - checks for lower casing
+ * Return : 0 if lower and 1 if positive
+ */
+
+int _islower(int c)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (c == low)
+		{
+			return (1)
+		}
+		else
+		{
+			return (0)
+		}
+	}
+}
