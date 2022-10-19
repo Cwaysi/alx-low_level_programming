c62b8e45368a2f977cdb0d66b9d4db28a7339df8
Cwaysi Morison <cwaysimorison@outlook.com>
2022-10-19 13:12:29 -0700

    pop

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - this is the function
+ * @c: this is the parameter
+ * Description: return 1 0 -1 when greater
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0);
+	{
+		_putchar('-,');
+		return (-1);
+	}
+}
+
