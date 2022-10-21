f7616fb85e6481713f674ff237f1f9d4597b3b1f
Cwaysi Morison <cwaysimorison@outlook.com>
2022-10-21 10:04:56 -0700

    king

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - function name
+ * @size: parameter
+ * Description: printing triangle
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int i = 0, j, n = size - 1;
+
+	if (size > 0)
+	{
+		for (; i < size; j++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
